e1d04d44fbcdd98bba688ae7fa1e4d9c90bb5414
chitranshuz <43988314+chitranshuz@users.noreply.github.com>
2023-03-13 17:49:07 +0530

    Update JFIFToPNGConverter.py
    cleanup

--- PNGconverter/JFIFToPNGConverter.py
+++ PNGconverter/JFIFToPNGConverter.py
@@ -1,8 +1,7 @@
 # import the lib
 # official link  https://pillow.readthedocs.io/en/stable/handbook/tutorial.html
-from PIL import Image, ImageFilter
+from PIL import Image
 import os
-from os import listdir
 import sys
 
 
@@ -10,18 +9,16 @@ import sys
 # SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
 # read the file and convert to png
 folder_dir = sys.argv[1]
+
+if not os.path.exists(sys.argv[2]):
+        os.mkdir(sys.argv[2])
+
 for images in os.listdir(folder_dir):
         file_name, extention = os.path.splitext(images)
         source_path = sys.argv[1] + images
-        print(file_name, extention)
         outfile = file_name + ".png"
         if images != outfile:
                 destination_file = sys.argv[2] + outfile
-                if os.path.exists(sys.argv[2]):
-                        pass
-                else:
-                        os.mkdir(sys.argv[2])
-
                 try:
                         with Image.open(source_path) as im:
                                  im.save(destination_file)
