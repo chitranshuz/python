e25f619f6815e95514b068450a3dbc9ad0c23753
chitranshuz <43988314+chitranshuz@users.noreply.github.com>
2023-03-17 21:49:01 +0530

    Update hackerNewScrap.py

--- dataScraping/hackerNewScrap.py
+++ dataScraping/hackerNewScrap.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+# request == browser once we have the object of the page, get the html format
 response = requests.get('https://news.ycombinator.com/news')
 http_page = BeautifulSoup(response.text, 'html.parser')
 
-
+# .titleline .subtext is the class of the CSS file which you can see in dev mode in chorme/ fn + F12, refer the README.md
 links = http_page.select('.titleline')
 score = http_page.select('.subtext')
 
